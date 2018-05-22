3d5b56eeafb958b56a5e3890604ccbfb347d1755
Zak Nesler <zaknesler@gmail.com>
2018-05-21 22:47:00 -0400

    Remove csrf token

--- src/stubs/views/default/welcome.blade.php
+++ src/stubs/views/default/welcome.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>{{ config('app.name') }}</title>
 
