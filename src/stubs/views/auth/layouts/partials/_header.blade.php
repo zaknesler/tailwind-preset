24027c26790b8719e27b71483f99866aa4dbead3
Zak Nesler <zaknesler@gmail.com>
2018-05-23 08:58:08 -0400

    Use dark menu icon

--- src/stubs/views/auth/layouts/partials/_header.blade.php
+++ src/stubs/views/auth/layouts/partials/_header.blade.php
@@ -7,7 +7,7 @@
         </div>
 
         <div class="block md:hidden">
-            <a @click.prevent="displayNavigation = !displayNavigation" class="flex items-center no-underline text-blue hover:text-blue-dark" href="#">
+            <a @click.prevent="displayNavigation = !displayNavigation" class="flex items-center no-underline text-blue-dark hover:text-blue" href="#">
                 <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </a>
         </div>
