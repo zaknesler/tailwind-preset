e4a6d40308e288ee2e41b70873199398681bc38a
Zak Nesler <zaknesler@gmail.com>
2018-05-24 11:41:35 -0400

    Add 503 error page

--- src/stubs/views/auth/errors/500.blade.php
+++ src/stubs/views/auth/errors/500.blade.php
@@ -6,8 +6,11 @@
 
 @section('content-full')
     <div class="h-full flex flex-col items-center justify-center text-center">
-        <div class="font-semibold text-2xl text-grey-darker">500</div>
-        <div class="text-grey-dark mt-3">Something went wrong.</div>
+        <div class="font-semibold text-3xl text-grey-darker">500</div>
+
+        <div class="block w-16 h-1 bg-blue my-6"></div>
+
+        <div class="text-lg text-grey-dark">Something went wrong.</div>
 
         <div class="mt-6">
             <a class="text-blue-dark no-underline hover:underline inline-flex items-center" href="{{ url()->previous() }}">
