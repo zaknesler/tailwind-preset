436290b0b69a5273bd82fdbb5b59c029e549a2c4
Zak Nesler <zaknesler@gmail.com>
2018-08-20 22:05:57 -0400

    Locale fix

--- src/stubs/views/auth/layouts/base.blade.php
+++ src/stubs/views/auth/layouts/base.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}" class="h-full">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
