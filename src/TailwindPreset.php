d8cf276b76327f6aedfdc30910b36fb9a0aebca0
Zak Nesler <zaknesler@gmail.com>
2020-04-30 18:55:08 -0400

    Update stubs

--- src/TailwindPreset.php
+++ src/TailwindPreset.php
@@ -73,6 +73,7 @@ class TailwindPreset extends Preset
             'welcome.stub',
         ]);
 
+        base_path('vendor/laravel/ui/stubs/migrations/2014_10_12_100000_create_password_resets_table.php')
         file_put_contents(app_path('Http/Controllers/HomeController.php'), static::compileControllerStub());
         copy(__DIR__.'/stubs/en.stub', resource_path('lang/en.json'));
     }
